8742340a4ebdb87cfc1f6c4c51c31ada016dfd80
AvaterClasher <avater.clasher@gmail.com>
2024-02-22 15:43:47 +0530

    feat: :sparkles: Added reset button to reset the conversation

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,5 +1,6 @@
 import { Message } from "@/types";
 import { FC } from "react";
+import { ResetChat } from "./ResetChat";
 
 interface Props {
     message: Message[];
@@ -10,7 +11,7 @@ export const Chat: FC<Props> = ({ message, loading }) => {
     return (
         <>
             <div className="flex flex-row justify-between items-center mb-4">
-                Reset
+                <ResetChat onReset={() => console.log("Reset")} />
             </div>
             <div className="mt-4 bottom-[56px] left-0 w-full">
                 Chatinput
