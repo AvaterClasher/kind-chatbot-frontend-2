686c86775d21485c25333efeea7770440449dd44
AvaterClasher <avater.clasher@gmail.com>
2024-02-22 15:33:12 +0530

    feat: :sparkles: Added Footer component

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import { FC } from "react";
+
+export const Footer: FC = () => {
+	return (
+		<div className="flex h-[30px] border-t border-neutral-300 py-2 px-8 items-center justify-center">Rustfully made by Soumyadip Moni 🦀</div>
+	);
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { GeistSans } from "geist/font/sans";
 import Head from "next/head";
 import { Navbar } from "@/components/Layout/Navbar";
+import { Footer } from "@/components/Layout/Footer";
 
 export default function Home() {
 	return (
@@ -18,6 +19,7 @@ export default function Home() {
 			<main className={`${GeistSans.className}`}>
         <div className="flex flex-col h-screen">
           <Navbar/>
+          <Footer/>
         </div>
       </main>;
 		</>
